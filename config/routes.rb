0a882dca8d0a5b9f8f162c6d14c1e326a5a85e07
524855 <annguyen@new-host-8.home>
2017-05-31 15:50:38 -0400

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,15 @@ Rails.application.routes.draw do
   resources :projects
   resources :users
   resources :features
+
+
+  root 'welcome#index' #set root
+
+
+  resources :user do
+    resource :projects
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
